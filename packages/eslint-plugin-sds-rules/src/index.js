63b2c7aeefa86da2f820b7e94a9ef410502f8495
Kishore Nemalipuri <lnemalipuri@salesforce.com>
2024-12-16 19:52:03 +0530

    Adding changes for eslint
    Signed-off-by: Kishore Nemalipuri <lnemalipuri@salesforce.com>

---
+++ packages/eslint-plugin-sds-rules/src/index.js
@@ -0,0 +1,17 @@
+console.log("Loading custom rules for eslint-plugin-sds-rules...");
+module.exports = {
+    rules: {
+        "no-bem-class": require("./rules/no-bem-class"),
+        "no-deprecated-slds-classes": require("./rules/no-deprecated-slds-classes")
+    },
+    configs: {
+        recommended: {
+            parser: "@html-eslint/parser", // Use HTML parser
+            plugins: ["sds-rules"],
+            rules: {
+                "sds-rules/no-bem-class": "error", // Enable rule
+                "sds-rules/no-deprecated-slds-classes": "error"
+            },
+        },
+    },
+};
