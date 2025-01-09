2d9ce7025fdc753b079781e2aa1c9955116423e0
Kishore Nemalipuri <lnemalipuri@salesforce.com>
2025-01-09 20:01:02 +0530

    Adding changes for typescript
    Signed-off-by: Kishore Nemalipuri <lnemalipuri@salesforce.com>

--- packages/sds-linter/index.mjs
+++
@@ -1,7 +0,0 @@
-import eslintPlugin from '@salesforce-ux/eslint-plugin-sf-sds';
-import stylelintPlugin from '@salesforce-ux/stylelint-sds';
-
-export default {
-  eslint: eslintPlugin,
-  stylelint: stylelintPlugin,
-};
