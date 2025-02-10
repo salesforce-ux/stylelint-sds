58bd294d12b9fba9149311c8ec468a479d7c1124
Kishore Nemalipuri <lnemalipuri@salesforce.com>
2025-02-10 15:59:05 +0530

    Adding a new rule to eslint
    Signed-off-by: Kishore Nemalipuri <lnemalipuri@salesforce.com>

--- packages/eslint-plugin-slds/src/index.js
+++ packages/eslint-plugin-slds/src/index.js
@@ -1,7 +1,8 @@
 module.exports = {
     rules: {
         "no-bem-class": require("./rules/no-bem-class"),
-        "no-deprecated-slds-classes": require("./rules/no-deprecated-slds-classes")
+        "no-deprecated-slds-classes": require("./rules/no-deprecated-slds-classes"),
+        "modal-close-button-issue": require('./rules/modal-close-button-issue')
     },
     configs: {
         recommended: {
@@ -9,7 +10,8 @@ module.exports = {
             plugins: ["slds"],  
             rules: {
                 "slds/no-bem-class": "error", 
-                "slds/no-deprecated-slds-classes": "error"
+                "slds/no-deprecated-slds-classes": "error",
+                "slds/modal-close-button-issue": "error"
             },
         },
     },
