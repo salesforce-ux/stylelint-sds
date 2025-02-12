a681367d4b0e51756f6c9d82a66de94a9bc83a47
sf-nithappu <nithappu@salesforce.com>
2025-02-12 23:58:52 +0530

    fix(lwc-usage): fixed issues in detection, reporting logic
    - corrected mapping table
    - corrected logic to detect --lwc usage
    - handled multiple style hook recommendations
    - handled raw value, calc, color-mix recommendations
    - added more test cases

--- packages/stylelint-plugin-slds/src/rules/lwc-to-slds-token/index.ts
+++ packages/stylelint-plugin-slds/src/rules/lwc-to-slds-token/index.ts
@@ -17,6 +17,11 @@ const messages = stylelint.utils.ruleMessages(ruleName, {
   replaced: (oldValue: string, newValue: string) =>
     replacePlaceholders(errorMsg, { oldValue, newValue }),
   warning: (oldValue: string) => replacePlaceholders(warningMsg, { oldValue }),
+
+  errorWithStyleHooks:(oldValue, newValue)=> `The '${oldValue}' design token is deprecated. To avoid breaking changes, replace it with the '${newValue}' styling hook and set the fallback to '${oldValue}'. For more info, see the New Global Styling Hook Guidance on lightningdesignsystem.com.\n\nOld Value: ${oldValue}\nNew Value: ${newValue}\n`,
+  errorWithNoRecommendation:(oldValue)=> `The '${oldValue}' design token is deprecated. For more info, see the New Global Styling Hook Guidance on lightningdesignsystem.com.`,
+  errorWithRawValue:(oldValue, newValue)=> `The '${oldValue}' design token is deprecated. To avoid breaking changes, replace it with '${newValue}'. For more info, see the New Global Styling Hook Guidance on lightningdesignsystem.com.\n\nOld Value: ${oldValue}\nNew Value: ${newValue}\n`,
+
 });
 
 const tokenMappingPath = metadataFileUrl('./public/metadata/lwcToSlds.json');
@@ -31,6 +36,11 @@ function validateOptions(result: PostcssResult, options: Options) {
   });
 }
 
+function isFixed(newValue ='', decl, parsedValue){
+  return newValue.startsWith('--slds-') &&
+              (decl.value.includes(newValue) || JSON.stringify(parsedValue).includes(newValue));
+}
+
 // Define the main rule logic
 function rule(
   primaryOptions: Options,
@@ -39,58 +49,77 @@ function rule(
 ) {
   return (root: Root, result: PostcssResult) => {
     if (validateOptions(result, primaryOptions)) {
+      
       root.walkDecls((decl) => {
         const parsedValue = valueParser(decl.value);
-
         parsedValue.walk((node) => {
           if (node.type === 'word' && node.value.startsWith('--lwc-')) {
             const oldValue = node.value;
-            const newValue =
-              lwcToSLDS[oldValue] === 'Continue to use'
-                ? ''
-                : lwcToSLDS[oldValue];
-            const startIndex = decl.toString().indexOf(decl.value);
-            const endIndex = startIndex + decl.value.length;
-            const severity =
-              result.stylelint.config.rules[ruleName]?.[1] ||
-              severityLevel; // Default to "error"
-            // Skip if the value has already been fixed
-            if (
-              newValue &&
-              newValue !== '--' &&
-              (decl.value.includes(newValue) ||
-                JSON.stringify(parsedValue).includes(newValue))
-            ) {
+
+            if(!(oldValue in lwcToSLDS) || lwcToSLDS[oldValue] === 'Continue to use'){
+              // Ignore if entry not found in the list or the token is marked to use further
               return;
             }
 
-            if (newValue && newValue !== '--') {
-              const proposedNewValue = `var(${newValue}, ${decl.value})`;
 
+            const recommendation = lwcToSLDS[oldValue];
+            const startIndex = decl.toString().indexOf(decl.value);
+            const endIndex = startIndex + decl.value.length;
+            const severity = result.stylelint.config.rules[ruleName]?.[1] || severityLevel; // Default to "error"
+            const hasRecommendation = recommendation && recommendation !== '--';
+            
+            if(!hasRecommendation){
+              // Found a deprecated token but don't have any alternate recommendation then just report user to follow docs
               stylelint.utils.report({
-                message: messages.replaced(oldValue, proposedNewValue),
+                message: messages.errorWithNoRecommendation(oldValue),
                 node: decl,
-                result,
                 index: startIndex,
                 endIndex,
+                result,
                 ruleName,
                 severity,
               });
+            }
 
-              // Fix if the context allows
-              if (result.stylelint.config.fix) {
-                decl.value = proposedNewValue;
+            // Skip if the value has already been fixed only if the recommendation starts with '--slds-'
+            if (hasRecommendation) {
+              // If there are multiple --slds- token recommendation for single --lwc we maintain recommendations as array
+              let checkFixed = Array.isArray(recommendation)?recommendation:[recommendation];
+              if(checkFixed.some(val => isFixed(val, decl, parsedValue))){
+                return;
               }
+            }
+
+            // Found recommendation, replace value
+            let proposedFix = '';
+            let canFix = true;
+            let message;
+            if(Array.isArray(recommendation)){
+              message = messages.errorWithStyleHooks(oldValue, recommendation.join(' or '))
+              canFix = false;
+            } else if(recommendation.startsWith('--slds-')){
+              message = messages.errorWithStyleHooks(oldValue, recommendation)
+              // add recommendation with fallback
+              proposedFix = `var(${recommendation}, ${decl.value})`;
             } else {
-              stylelint.utils.report({
-                message: messages.warning(oldValue),
-                node: decl,
-                index: startIndex,
-                endIndex,
-                result,
-                ruleName,
-                severity,
-              });
+              message = messages.errorWithRawValue(oldValue, recommendation)
+              // for any raw values, color-mix, calc
+              proposedFix = recommendation;
+            }        
+
+            stylelint.utils.report({
+              message,
+              node: decl,
+              result,
+              index: startIndex,
+              endIndex,
+              ruleName,
+              severity,
+            });
+
+            // Fix if the context allows
+            if (result.stylelint.config.fix && canFix) {
+              decl.value = proposedFix;
             }
           }
         });
