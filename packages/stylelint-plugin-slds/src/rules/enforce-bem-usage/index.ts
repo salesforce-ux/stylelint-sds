4301af5aa493b0d9bf88040945023fef0351182d
Heber <Anthony.Heber@gmail.com>
2025-02-11 19:31:56 -0700

    stylelint-plugin-slds: enhance enforce-bem-usage rule
    Adds a new dependency, postcss-selector-parser, so we can breakdown the selectors and fish out the class names.
    
    Add support for much more complicated selectors and add supporting tests.

--- packages/stylelint-plugin-slds/src/rules/enforce-bem-usage/index.ts
+++ packages/stylelint-plugin-slds/src/rules/enforce-bem-usage/index.ts
@@ -1,16 +1,22 @@
 import stylelint, { Rule, PostcssResult } from 'stylelint';
 import { readFileSync } from 'fs';
 import { Root } from 'postcss';
+import SelectorParser from 'postcss-selector-parser';
 import { parse } from 'yaml';
 import { metadataFileUrl } from '../../utils/metaDataFileUrl';
 const { utils, createPlugin } = stylelint;
 import ruleMetadata from './../../utils/rulesMetadata';
 import replacePlaceholders from '../../utils/util';
 
+const ruleName: string = 'slds/enforce-bem-usage';
+const selectorParser = SelectorParser();
 
-const ruleName:string = 'slds/enforce-bem-usage';
-
-const { severityLevel = 'error', warningMsg = '', errorMsg = '', ruleDesc = 'No description provided' } = ruleMetadata(ruleName) || {};
+const {
+  severityLevel = 'error',
+  warningMsg = '',
+  errorMsg = '',
+  ruleDesc = 'No description provided',
+} = ruleMetadata(ruleName) || {};
 
 interface Item {
   name: string;
@@ -26,8 +32,8 @@ interface ParsedData {
 
 const messages = stylelint.utils.ruleMessages(ruleName, {
   replaced: (oldValue: string, newValue: string) =>
-    replacePlaceholders(errorMsg, { oldValue, newValue} )
-    //`Consider updating '${oldValue}' to new naming convention '${newValue}'`,
+    replacePlaceholders(errorMsg, { oldValue, newValue }),
+  //`Consider updating '${oldValue}' to new naming convention '${newValue}'`,
 });
 
 const isTestEnv = process.env.NODE_ENV === 'test';
@@ -44,31 +50,39 @@ function validateOptions(result: PostcssResult, options: any): boolean {
 
 function rule(primaryOptions?: any) {
   return (root: Root, result: PostcssResult) => {
-    if (validateOptions(result, primaryOptions))
-    {
+    if (validateOptions(result, primaryOptions)) {
       root.walkRules((rule) => {
-        const givenProp = rule.selector.replace('.', ''); //to remove cases like .slds-text-heading_large
-        if (bemMapping.items[0].tokens[givenProp]) {
-          const index = rule.toString().indexOf(givenProp);
-          const endIndex = index + givenProp.length;
-          const newValue = bemMapping.items[0].tokens[givenProp];
-          const severity =
+        let fixOffset = 0; // aggregate position change if using auto-fix, tracked at the rule level
+        for (const classNode of getClassesFromSelector(rule.selector)) {
+          // check mapping data for this class name
+          const newValue = bemMapping.items[0].tokens[classNode.value];
+          if (newValue) {
+            const index =
+              rule.toString().indexOf(rule.selector) +
+              classNode.sourceIndex +
+              1; // find selector in rule plus '.'
+            const endIndex = index + classNode.value.length;
+            const severity =
               result.stylelint.config.rules[ruleName]?.[1] || severityLevel; // Default to "error"
-          
-          if (typeof newValue === 'string') {
-            stylelint.utils.report({
-              message: messages.replaced(givenProp, newValue),
-              node: rule,
-              index,
-              endIndex,
-              result,
-              ruleName,
-              severity
-            });
+            const fix = () => {
+              rule.selector =
+                rule.selector.substring(0, fixOffset + index) +
+                newValue +
+                rule.selector.substring(fixOffset + endIndex);
+              fixOffset += newValue.length - (endIndex - index);
+            };
 
-            // Call the fix method if in fixing context
-            if (result.stylelint.config.fix) {
-              fix(rule, newValue);
+            if (typeof newValue === 'string') {
+              stylelint.utils.report({
+                message: messages.replaced(classNode.value, newValue),
+                node: rule,
+                index,
+                endIndex,
+                result,
+                ruleName,
+                severity,
+                fix,
+              });
             }
           }
         }
@@ -77,8 +91,15 @@ function rule(primaryOptions?: any) {
   };
 }
 
-function fix(decl: any, newValue: string): void {
-  decl.value = newValue; // Update the declaration value
+function getClassesFromSelector(selector: string) {
+  const selectorAst = selectorParser.astSync(selector);
+  const classNames = [];
+  selectorAst.walkClasses((classNode) => {
+    classNames.push(classNode);
+  });
+  return classNames;
 }
+
+rule.meta = { ruleName, messages, fixable: true };
 
 export default createPlugin(ruleName, rule as unknown as Rule);
