59bbab0c18ca6f98ed0462da5e6d0b9336b0d816
Naveen Ithappu <nithappu@salesforce.com>
2025-02-14 18:11:11 +0530

    fix(deprecared-slds-classes): updated metadata
    - updated json file
    - updated testcases

--- packages/stylelint-plugin-slds/src/rules/no-deprecated-slds-classes/index.ts
+++ packages/stylelint-plugin-slds/src/rules/no-deprecated-slds-classes/index.ts
@@ -4,21 +4,27 @@ import { Root } from 'postcss';
 import { metadataFileUrl } from '../../utils/metaDataFileUrl';
 import ruleMetadata from '../../utils/rulesMetadata';
 import replacePlaceholders from '../../utils/util';
+import { getClassNodesFromSelector } from '../../utils/selector-utils';
 
 const { utils, createPlugin } = stylelint;
 
+const ruleName: string = 'slds/no-deprecated-slds-classes';
 
-
-const ruleName:string = 'slds/no-deprecated-slds-classes';
-
-const { severityLevel = 'error', warningMsg = '', errorMsg = '', ruleDesc = 'No description provided' } = ruleMetadata(ruleName) || {};
+const {
+  severityLevel = 'error',
+  warningMsg = '',
+  errorMsg = '',
+  ruleDesc = 'No description provided',
+} = ruleMetadata(ruleName) || {};
 const messages = stylelint.utils.ruleMessages(ruleName, {
   deprecated: (className: string) =>
-    replacePlaceholders(errorMsg,{className}),
+    replacePlaceholders(errorMsg, { className }),
 });
 
 const isTestEnv = process.env.NODE_ENV === 'test';
-const tokenMappingPath = metadataFileUrl('./public/metadata/deprecatedClasses.json');
+const tokenMappingPath = metadataFileUrl(
+  './public/metadata/deprecatedClasses.json'
+);
 
 const defaultDeprecatedClasses = new Set(
   JSON.parse(readFileSync(tokenMappingPath, 'utf8'))
@@ -41,27 +47,32 @@ function rule(
   deprecatedClasses = defaultDeprecatedClasses
 ) {
   return (root: Root, result: PostcssResult) => {
-    if (validateOptions(result, primaryOptions)) {
-      root.walkRules((rule) => {
-        const classMatches = rule.selector.match(classRegex);
-        const severity =
-                      result.stylelint.config.rules[ruleName]?.[1] || severityLevel; // Default to "error"
-        if (classMatches) {
-          classMatches.forEach((match) => {
-            const className = match.slice(1);
-            if (deprecatedClasses.has(className)) {
-              utils.report({
-                message: messages.deprecated(className),
-                node: rule,
-                result,
-                ruleName,
-                severity
-              });
-            }
-          });
-        }
-      });
+    const severity =
+      result.stylelint.config.rules[ruleName]?.[1] || severityLevel; // Default to "error"
+    if (!validateOptions(result, primaryOptions)) {
+      return;
     }
+
+    root.walkRules((rule) => {
+      const classNodes = getClassNodesFromSelector(rule.selector);
+      const offsetIndex = rule.toString().indexOf(rule.selector);
+      classNodes.forEach((classNode) => {
+        if (!deprecatedClasses.has(classNode.value)) {
+          return;
+        }
+        const index = offsetIndex + classNode.sourceIndex + 1; // find selector in rule plus '.'
+        const endIndex = index + classNode.value.length;
+        utils.report({
+          message: messages.deprecated(classNode.value),
+          node: rule,
+          result,
+          ruleName,
+          severity,
+          index,
+          endIndex
+        });
+      });
+    });
   };
 }
 
--- packages/stylelint-plugin-slds/tests/rules/no-deprecated-slds-classes/no-deprecated-slds-classes.spec.ts
+++ packages/stylelint-plugin-slds/tests/rules/no-deprecated-slds-classes/no-deprecated-slds-classes.spec.ts
@@ -2,27 +2,41 @@ import { expect } from 'chai';
 import stylelint, { LinterResult, LinterOptions } from 'stylelint';
 const { lint }: typeof stylelint = stylelint;
 
-describe('slds/no-deprecated-slds-classes', () => {
-  const messages = [
-    'The class "slds-box_border" is deprecated and not available in SLDS2. Please update to a supported class. (slds/no-deprecated-slds-classes)',
-    'The class "slds-chat-avatar__intials" is deprecated and not available in SLDS2. Please update to a supported class. (slds/no-deprecated-slds-classes)',
-  ];
+describe('slds/no-deprecated-slds-classes', async () => {
 
-  messages.map((message, index) => {
-    it(`test rule #${index}`, async () => {
-      const linterResult: LinterResult = await lint({
-        files: './tests/providers/no-deprecated-slds-classes.css',
-        config: {
-          plugins: ['./src/index.ts'],
-          rules: {
-            'slds/no-deprecated-slds-classes': true,
-          },
+
+  const tests = [{
+    description:"Should report slds-box_border as deprecated",
+    input:'.slds-box_border{border:0}',
+    expectedMessage:'The class "slds-box_border" is deprecated and not available in SLDS2. Please update to a supported class. (slds/no-deprecated-slds-classes)'
+  }, {
+    description:"Should not report slds-custom-created",
+    input:'.slds-custom-created{border:0}'
+  }]
+
+  const testStyleLintRule = async (code:string) => {
+    const linterResult: LinterResult = await lint({
+      code,
+      config: {
+        plugins: ['./src/index.ts'],
+        rules: {
+          'slds/no-deprecated-slds-classes': true,
         },
-      } as LinterOptions);
+      },
+    } as LinterOptions);
+  
+    return linterResult.results[0].warnings.map((warning) => warning.text);
+  };
 
-      expect(
-        linterResult.results.at(0)?._postcssResult.messages[index]?.text
-      ).to.equal(message);
-    });
+  tests.forEach(({description, input, expectedMessage})=>{
+    it(description, async ()=>{
+       const messages = await testStyleLintRule(input)
+       if(expectedMessage){
+        expect(messages[0]).to.include(expectedMessage);
+       } else {
+        expect(messages).to.be.empty;
+       }
+    })
   });
+
 });
