0f10a9f5cbdbc3ce6647d0e0521268760ae8a2b2
Kenam Verma <kenam.verma@kenamve-ltmpymq.internal.salesforce.com>
2025-02-17 09:34:57 +0530

    fix: fix slds overrides

--- packages/stylelint-plugin-slds/src/rules/no-slds-class-overrides/index.ts
+++ packages/stylelint-plugin-slds/src/rules/no-slds-class-overrides/index.ts
@@ -5,6 +5,8 @@ import { Options } from './option.interface';
 import ruleMetadata from '../../utils/rulesMetadata';
 import replacePlaceholders from '../../utils/util';
 import { getClassNodesFromSelector } from '../../utils/selector-utils';
+import { metadataFileUrl } from '../../utils/metaDataFileUrl';
+import { readFileSync } from 'fs';
 const { utils, createPlugin }: typeof stylelint = stylelint;
 
 const ruleName: string = 'slds/no-slds-class-overrides';
@@ -16,6 +18,9 @@ const {
   ruleDesc = 'No description provided',
 } = ruleMetadata(ruleName) || {};
 
+const tokenMappingPath = metadataFileUrl('./public/metadata/slds_classes.json');
+const sldsClasses = JSON.parse(readFileSync(tokenMappingPath, 'utf8'));
+
 function rule(primaryOptions: Options) {
   return (root: Root, result: PostcssResult) => {
     const severity =
@@ -23,27 +28,30 @@ function rule(primaryOptions: Options) {
 
     root.walkRules((rule) => {
       const classNodes = getClassNodesFromSelector(rule.selector);
+      
       const offsetIndex = rule.toString().indexOf(rule.selector);
       classNodes.forEach((classNode) => {
-    
+        
         if(!classNode.value.startsWith('slds-')){
           // Ignore if the selector do not start with `slds-*`
           return;
         } else {
-          //TODO: match against slds_classes.json entries. As of now we have 4k_ entries. Matching will be and expensive operation
-          // We live with bug, this rule reports custome created slds class aswell. example .slds-my-own will be reported.
+          //match against slds_classes.json entries. As of now we have 4k_ entries.
+          if(sldsClasses.indexOf(classNode.value) > -1)
+          {
+            const index = offsetIndex + classNode.sourceIndex + 1; // find selector in rule plus '.'
+            const endIndex = index + classNode.value.length;
+            utils.report({
+              message: replacePlaceholders(errorMsg,{selector:`.${classNode.value}`}),
+              node: rule,
+              result,
+              ruleName,
+              severity,
+              index,
+              endIndex,
+            });
+          }
         }
-        const index = offsetIndex + classNode.sourceIndex + 1; // find selector in rule plus '.'
-        const endIndex = index + classNode.value.length;
-        utils.report({
-          message: replacePlaceholders(errorMsg,{selector:`.${classNode.value}`}),
-          node: rule,
-          result,
-          ruleName,
-          severity,
-          index,
-          endIndex,
-        });
       });
     });
   };
