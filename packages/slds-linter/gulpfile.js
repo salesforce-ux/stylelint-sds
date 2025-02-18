89e56e7ae9dbc1bdc014fb287b9b60601908c267
Naveen Ithappu <nithappu@salesforce.com>
2025-02-18 15:13:52 +0530

    package changes to support integration

---
+++ packages/slds-linter/gulpfile.js
@@ -0,0 +1,20 @@
+import { series, src, dest } from 'gulp';
+import { rimraf} from 'rimraf'
+import {task} from "gulp-execa";
+
+/**
+ * Clean all generated folder
+ * @returns 
+ */
+function cleanDirs(){
+    return rimraf(['build']);
+}
+
+ /**
+  * Compile typescript files
+  * */ 
+const compileTs = task(`tsc --project ${process.cwd()}/tsconfig.json`);
+  
+export const build = series(cleanDirs, compileTs);
+
+export default task('gulp --tasks');
