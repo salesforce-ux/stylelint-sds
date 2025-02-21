543a7c290715703a4a9cc3ade34541c23fb67e2a
Naveen Ithappu <nithappu@salesforce.com>
2025-02-21 13:13:52 +0530

    chore(script): corrected build scripts to bundle metadata package
    - generating tokens-data file with imports to json
    - generating definition files
    - bundling with esbuild
    - corrected tsconfig.jso

--- packages/metadata/gulpfile.js
+++ packages/metadata/gulpfile.js
@@ -1,56 +1,13 @@
-import { Transform } from "node:stream";
-import { Buffer } from "node:buffer";
-import { pipeline } from "node:stream/promises";
-import { series, src, dest } from "gulp";
-import { rimraf } from "rimraf";
-import { task } from "gulp-execa";
-import { basename, parse, format } from "node:path";
 import { camelCase } from "change-case";
-import { glob } from "glob";
-import { writeFileSync } from "node:fs";
 import * as esbuild from "esbuild";
-import {normalizeData} from "./scripts/sldshooks.metadata.parser.js";
-
-/**
- * Gulp task to pipe file content into a js file with a named export
- * name of the export will be camelCase of file's base name
- *
- * @returns
- */
-function jsonToJsExport() {
-  const exportToJson = new Transform({ objectMode: true });
-  exportToJson._transform = (originalFile, encoding, callback) => {
-    var file = originalFile.clone({ contents: false });
-    const pathParts = parse(file.path);
-    const exportName = camelCase(pathParts.name);
-    pathParts.ext = "js";
-    pathParts.name = exportName;
-    delete pathParts.base;
-    file.path = format(pathParts);
-    file.contents = Buffer.from(
-      `export const ${exportName} =${JSON.stringify(JSON.parse(file.contents), null, 4)}`
-    );
-    callback(null, file);
-  };
-  return exportToJson;
-}
-
-async function generateIndex() {
-  const listOfFiles = await glob(`./resources/*.json`);
-  const content = listOfFiles
-    .map((filePath) => {
-      return `export * from "./${camelCase(basename(filePath, ".json"))}.js";`;
-    })
-    .join("\n");
-  writeFileSync("./build/index.js", content);
-
-  writeFileSync(
-    "./build/index.d.ts",
-    `declare module '@salesforce-ux/matadata-slds';
-    ${content}
-  `
-  );
-}
+import { esbuildPluginFilePathExtensions } from "esbuild-plugin-file-path-extensions";
+import { glob } from "glob";
+import { series } from "gulp";
+import { task } from "gulp-execa";
+import { writeFileSync } from "node:fs";
+import { parse, relative } from "node:path";
+import { rimraf } from "rimraf";
+import { normalizeData } from "./.scripts/sldshooks.metadata.parser.mjs";
 
 /**
  * Clean all generated folder
@@ -60,14 +17,48 @@ function cleanDirs() {
   return rimraf(["build"]);
 }
 
+async function generateTokensDataFile() {
+  const listOfFiles = await glob(`./resources/*.json`);
+  const namedExports = [];
+  const content = listOfFiles
+    .map((filePath) => {
+      const fileNameParts = parse(filePath);
+      const importName = camelCase(fileNameParts.name);
+      const importPath = relative("./src", filePath);
+      namedExports.push(importName);
+      return `import ${importName} from "${importPath}" with {type:"json"};`;
+    })
+    .join("\n");
+
+  writeFileSync(
+    "./src/tokens-data.ts",
+    `
 /**
- * Compile typescript files
- * */
-function generateFiles() {
-  return pipeline(src(`./resources/*.json`), jsonToJsExport(), dest("build/"));
+ *  This file is auto-generated from script
+ *  DO NOT EDIT
+ */
+${content}
+
+export {${namedExports.sort().join(",")}}
+    `
+  );
 }
 
-const genrateDefinitions = task(`tsc --project ./tsconfig.json`);
+async function generateBundle() {
+  await esbuild.build({
+    entryPoints: ["./src/*.ts"],
+    bundle:true,
+    outdir:"build",
+    platform: "node",
+    format:"esm",
+    packages:'external',
+    plugins:[esbuildPluginFilePathExtensions({
+      esmExtension:"js"
+    })]
+  })  
+}
+
+const generateDefinitions = task('tsc --project tsconfig.json');
 
 /**
  * This method uses resources/globalSharedHooks.metadata.json to generate value --> hooks mapping for bothe slds and slds-plus
@@ -80,9 +71,9 @@ export async function value2HooksMappings() {
 export const build = series(
   cleanDirs,
   value2HooksMappings,
-  generateFiles,
-  genrateDefinitions,
-  generateIndex
+  generateTokensDataFile,
+  generateBundle,
+  generateDefinitions
 );
 
 export default task("gulp --tasks");
