fdf8152f486e828183d98f22b21659f27b118eb3
Naveen Ithappu <nithappu@salesforce.com>
2025-02-21 13:13:52 +0530

    chore(script): corrected build scripts to bundle metadata package
    - generating tokens-data file with imports to json
    - generating definition files
    - bundling with esbuild
    - corrected tsconfig.jso

---
+++ packages/metadata/src/tokens-data.ts
@@ -0,0 +1,19 @@
+
+/**
+ *  This file is auto-generated from script
+ *  DO NOT EDIT
+ */
+import valueToStylinghookSldsplus from "../resources/valueToStylinghook.sldsplus.json" with {type:"json"};
+import valueToStylinghookSlds from "../resources/valueToStylinghook.slds.json" with {type:"json"};
+import utilities from "../resources/utilities.json" with {type:"json"};
+import tokenMapping from "../resources/tokenMapping.json" with {type:"json"};
+import sldsClasses from "../resources/slds_classes.json" with {type:"json"};
+import sldsPlusMetadata from "../resources/sldsPlus.metadata.json" with {type:"json"};
+import lwcToSlds from "../resources/lwcToSLDS.json" with {type:"json"};
+import globalSharedHooksMetadata from "../resources/globalSharedHooks.metadata.json" with {type:"json"};
+import deprecatedHooks from "../resources/deprecatedHooks.json" with {type:"json"};
+import deprecatedClasses from "../resources/deprecatedClasses.json" with {type:"json"};
+import bemNaming from "../resources/bem-naming.json" with {type:"json"};
+
+export {bemNaming,deprecatedClasses,deprecatedHooks,globalSharedHooksMetadata,lwcToSlds,sldsClasses,sldsPlusMetadata,tokenMapping,utilities,valueToStylinghookSlds,valueToStylinghookSldsplus}
+    
