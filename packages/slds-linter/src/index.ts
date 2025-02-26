cfce3ca94c868e85a1c93b85deed8d31bc7661b0
Naveen Ithappu <nithappu@salesforce.com>
2025-02-26 17:11:25 +0530

    feat(slds-lint): added emit command to create config files

--- packages/slds-linter/src/index.ts
+++ packages/slds-linter/src/index.ts
@@ -5,6 +5,7 @@ import { registerLintStylesCommand } from './commands/lint-styles';
 import { registerLintComponentsCommand } from './commands/lint-components';
 import { registerLintCommand } from './commands/lint';
 import { registerReportCommand } from './commands/report';
+import { registerEmitCommand } from './commands/emit';
 import { Logger } from './utils/logger';
 import { validateNodeVersion } from './utils/nodeVersionUtil';
 import pkg from '../package.json' with {type:"json"};
@@ -34,5 +35,6 @@ registerLintStylesCommand(program);
 registerLintComponentsCommand(program);
 registerLintCommand(program);
 registerReportCommand(program);
+registerEmitCommand(program);
 
 program.parse(process.argv); 
