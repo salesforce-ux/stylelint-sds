a3a6b11f9150186225e3e7f03a05f91f16416825
Kenam Verma <kenam.verma@salesforce.com>
2025-02-27 16:15:07 +0530

    fix: fix nits

--- packages/cli/src/commands/lint-styles.ts
+++ packages/cli/src/commands/lint-styles.ts
@@ -38,7 +38,7 @@ export function registerLintStylesCommand(program: Command): void {
         Logger.info(chalk.blue('Running stylelint...'));
         const results = await LintRunner.runLinting(fileBatches, 'style', {
           fix: normalizedOptions.fix,
-          configPath: normalizedOptions.config,
+          configPath: normalizedOptions.configStyle,
         });
 
         // Print detailed lint results only for files with issues
