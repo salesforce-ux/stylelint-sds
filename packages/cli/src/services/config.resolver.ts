689f5583cb37c4e7dffda957604822437c95f549
Naveen Ithappu <nithappu@salesforce.com>
2025-03-06 16:12:58 +0530

    fix(cli): added support for legacy node version
    - using import-meta-resolve as polyfill for node 16-20
    - version check at the start, warns if the required version not met
    - updated read me

--- packages/cli/src/services/config.resolver.ts
+++ packages/cli/src/services/config.resolver.ts
@@ -1,9 +1,9 @@
-import { fileURLToPath } from "url";
 // TODO:Move rule meta to metadata package
 import {ruleMetadata} from '@salesforce-ux/stylelint-plugin-slds';
+import { resolvePath } from '../utils/nodeVersionUtil';
 
-export const DEFAULT_ESLINT_CONFIG_PATH = fileURLToPath(import.meta.resolve('@salesforce-ux/eslint-plugin-slds/.eslintrc.yml'));
-export const DEFAULT_STYLELINT_CONFIG_PATH = fileURLToPath(import.meta.resolve('@salesforce-ux/stylelint-plugin-slds/.stylelintrc.yml'));
+export const DEFAULT_ESLINT_CONFIG_PATH = resolvePath('@salesforce-ux/eslint-plugin-slds/.eslintrc.yml', import.meta);
+export const DEFAULT_STYLELINT_CONFIG_PATH = resolvePath('@salesforce-ux/stylelint-plugin-slds/.stylelintrc.yml', import.meta);
 export const STYLELINT_VERSION = process.env.STYLELINT_VERSION;
 export const ESLINT_VERSION = process.env.ESLINT_VERSION;
 export const LINTER_CLI_VERSION = process.env.CLI_VERSION;
